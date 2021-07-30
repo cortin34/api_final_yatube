5234a6e29417839eef9f77c4bc5a084a7efb936d
yandex-praktikum <58176914+yandex-praktikum@users.noreply.github.com>
2021-07-30 20:08:10 +0300

    removed group

--- yatube_api/posts/migrations/0001_initial.py
+++ yatube_api/posts/migrations/0001_initial.py
@@ -13,15 +13,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Group',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('slug', models.SlugField(unique=True)),
-                ('description', models.TextField()),
-            ],
-        ),
-        migrations.CreateModel(
             name='Post',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
